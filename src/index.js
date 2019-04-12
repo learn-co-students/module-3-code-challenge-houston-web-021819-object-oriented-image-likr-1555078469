2eb609abb825fd148090b5e2d32df02428800364
Mark R Pothecary <markpothers@hotmail.com>
2019-04-12 09:30:29 -0500

    fetch working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 1 //Enter the id from the fetched image here  https://randopic.herokuapp.com/images/2440
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageURL = `https://randopic.herokuapp.com/images/2440`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
+  fetch(imageURL)
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(result){
+    console.log(result)
+  })
+
+
 
 
 })
