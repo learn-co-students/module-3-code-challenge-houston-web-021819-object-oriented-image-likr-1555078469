c4ace0e17aa106e0b310213373f5c7aa042208f1
github username <github email address>
2019-04-12 11:24:12 -0500

    Done

---
+++ src/Comment.js
@@ -0,0 +1,8 @@
+class Comment{
+    constructor(content){
+        const commentCon = document.querySelector('#comments')
+        const commentTag = document.createElement('li')
+        commentCon.append(commentTag)
+        commentCon.innerText = comment.value
+    }
+}
