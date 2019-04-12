d993a6135fcda11e22f30dbfad993bafaad99e82
Jessica Brumfield <jessicabrumfield@Jessicas-MacBook-Pro.local>
2019-04-12 09:37:55 -0500

    fetched and appended image to the DOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  const imageId = 2441 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  fetch(imageURL)
+  .then(function(res){
+    return res.json()
+  })
+  .then(function(pic){
+    const picPlace = document.querySelector('#image_container')
+    console.log(pic)
+    let bigPic = new Image(pic.url,pic.name)
+    picPlace.append(bigPic.div)
+  })
 
 
 })
